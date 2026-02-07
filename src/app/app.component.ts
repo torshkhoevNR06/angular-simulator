9b8a86ffef140ff6c18c2e65f1c3f9dc1e994d7a
Nakir <nakir3000@gmail.com>
2026-02-07 16:01:25 +0300

    fix-6-hw-16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   selectedLocation!: boolean;
   selectedDate!: boolean;
   selectedParticipants!: boolean;
-  selectedInput!: string;
+  liveTextInput!: string;
   dateTime!: string;
   counter: number = 0;
   currentTask!: 'counter' | 'dateTime';
