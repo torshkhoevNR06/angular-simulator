224749bc8c04890bb1fd72c3cd07d68b024f34c1
Nakir <nakir3000@gmail.com>
2026-07-21 16:28:50 +0300

    fix-2-27-hw

--- src/format-date.token.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { DatePipeConfig } from '@angular/common';
-
-export const DATE_PIPE_DEFAULT_OPTIONS: InjectionToken<DatePipeConfig> = new InjectionToken<DatePipeConfig>('DATE_PIPE_DEFAULT_OPTIONS');
